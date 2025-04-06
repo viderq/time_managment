cac4c54f7e91496c11fe07a693172893fc7a6a9f
viderq <102806262+viderq@users.noreply.github.com>
2025-04-06 21:44:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
       .then(reg => console.log('Service Worker зарегистрирован:', reg))
       .catch(err => console.error('Ошибка регистрации Service Worker:', err));
   });
